f21108f4e0e328f62312d6ad7ff2e187ce10c4d3
DanielBouless <dbouless76@gmail.com>
2022-12-03 01:02:32 -0500

    i wish i could remember

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import SignUp from './pages/SignUp';
 
 function App() {
+
+const styleApp = {
+  backgroundColor: "#282c34",
+  color: 'white',
+  height: '100vh',
+  textAlign: 'center',
+
+}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={styleApp}>
+      <SignUp/>
     </div>
   );
 }
